cf254b876ceac57326c9f3a2bf1956fda4894e24
Ali Mohammadi Barzegar <a_barzegar@modernisc.com>
2024-02-05 15:28:01 +0330

    HTTP status codes

--- lets-go/snippetbox/main.go
+++ lets-go/snippetbox/main.go
@@ -19,7 +19,19 @@ func snippetView(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Display a specific snippet..."))
 }
 
+//func snippetCreate(w http.ResponseWriter, r *http.Request) {
+//	w.Write([]byte("Create a new snippet..."))
+//}
+
 func snippetCreate(w http.ResponseWriter, r *http.Request) {
+
+	if r.Method != "POST" {
+		w.WriteHeader(405)
+		w.Write([]byte("Method Not Allowed"))
+		return
+
+	}
+
 	w.Write([]byte("Create a new snippet..."))
 }
 
