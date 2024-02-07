eea08467a6bed2f77af201a7d43d0ad59c5ea539
ali mohammadi <ali90090021@gmail.com>
2024-02-07 13:25:14 +0330

    Managing configuration settings-68

--- lets-go/snippetbox/cmd/web/main.go
+++ lets-go/snippetbox/cmd/web/main.go
@@ -1,19 +1,31 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
 )
 
 func main() {
 
+	//	addr := flag.String("addr", ":4000", "HTTP network address")
+
+	var cfg config
+	flag.StringVar(&cfg.addr, "addr", ":4000", "hTTP network address")
+
+	flag.Parse()
+
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("/snippet/view", snippetView)
 	mux.HandleFunc("/snippet/create", snippetCreate)
 
-	log.Print("Starting Server")
-	err := http.ListenAndServe(":4000", mux)
+	log.Print("Starting Server on %s", cfg.addr)
+	err := http.ListenAndServe(cfg.addr, mux)
 	log.Fatal(err)
 
+}
+
+type config struct {
+	addr string
 }
